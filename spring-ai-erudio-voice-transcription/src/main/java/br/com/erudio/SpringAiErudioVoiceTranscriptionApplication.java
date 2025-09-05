6cbd43bf2976295c0ee7fd8727a961e4d68b4a4c
Josaf4 <josafa.soares2909@gmail.com>
2025-09-05 16:17:18 -0400

    Commit

---
+++ spring-ai-erudio-voice-transcription/src/main/java/br/com/erudio/SpringAiErudioVoiceTranscriptionApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiErudioVoiceTranscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiErudioVoiceTranscriptionApplication.class, args);
+	}
+
+}
